a50bc2dade145825d0dd21bf18fd8d08282d5952
Sicu Bogdan-Andrei <b0gdan.munt3@gmail.com>
2024-02-14 16:35:35 +0200

    added entity and first request

--- src/feed/models/post.entity.ts
+++ src/feed/models/post.entity.ts
@@ -1,6 +1,13 @@
-import { Entity } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
 @Entity('feed_post')
 export class FeedPostEntity {
-    
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({default: '' })
+    body: string;
+
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    createdAt: Date;
 }
--- src/feed/services/feed.service.ts
+++ src/feed/services/feed.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { FeedPostEntity } from '../models/post.entity';
+import { Repository } from 'typeorm'
+import { InjectRepository } from '@nestjs/typeorm';
+import { FeedPost } from '../models/post.interface';
+import { Observable, from } from 'rxjs';
 
 @Injectable()
-export class FeedService {}
+export class FeedService {
+
+    constructor(
+        @InjectRepository(FeedPostEntity)
+        private readonly feedPostRepository: Repository<FeedPostEntity>
+    ) {}
+
+    createPost(feedPost: FeedPost): Observable<FeedPost> {
+        return from(this.feedPostRepository.save(feedPost));
+    }
+
+}
